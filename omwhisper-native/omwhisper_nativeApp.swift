e1c2479d249812ac16f95a299158b1b8b0cdec3d
Rakesh Kusuma <rockykusuma@gmail.com>
2026-07-06 19:06:28 +0530

    first commit

---
+++ omwhisper-native/omwhisper_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  omwhisper_nativeApp.swift
+//  omwhisper-native
+//
+//  Created by Rakesh Kusuma on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct omwhisper_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ omwhisper-nativeTests/omwhisper_nativeTests.swift
@@ -0,0 +1,19 @@
+//
+//  omwhisper_nativeTests.swift
+//  omwhisper-nativeTests
+//
+//  Created by Rakesh Kusuma on 06/07/26.
+//
+
+import Testing
+@testable import omwhisper_native
+
+struct omwhisper_nativeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
